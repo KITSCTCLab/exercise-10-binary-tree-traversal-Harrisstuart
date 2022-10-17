80db3e49792762ab2d5b7993a2529d7bd9ca535b
Harrisstuart <109952207+Harrisstuart@users.noreply.github.com>
2022-10-17 14:52:51 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,46 +1,59 @@
-from typing import List
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
 
-def merge(arr: [int], l: int, mid: int, r: int) -> [int]:
-  l1 = arr[l:mid]
-  l2 = arr[mid:r]
-  i = 0
-  j = 0
-  k = l
-  while i < len(l1) and j < len(l2):
-    if l1[i] < l2[j]:
-      arr[k] = l1[i]
-      i += 1
-    else:
-      arr[k] = l2[j]
-      j += 1
-    k += 1
-  while i < len(l1):
-    arr[k] = l1[i]
-    i += 1
-    k += 1
-  while j < len(l2):
-    arr[k] - l2[j]
-    j += 1
-    k += 1
 
-def merge_sort_ftn(arr, l, r) -> None:
-  if r - l >= 2:
-    mid = l + (r - l) // 2
-    merge_sort_ftn(arr, l, mid)
-    merge_sort_ftn(arr, mid, r)
-    merge(arr, l, mid, r)
-  
-def merge_sort(data) -> None:
-  merge_sort_ftn(data, 0, len(data))
+def insert(root, new_value) -> BinaryTreeNode:
+    """If binary search tree is empty, make a new node, declare it as root and return the root.
+        If tree is not empty and if new_value is less than value of data in root, add it to left subtree and proceed recursively.
+        If tree is not empty and if new_value is >= value of data in root, add it to right subtree and proceed recursively.
+        Finally, return the root.
+        """
+    # Write your code here
+    if root == None:
+        root = BinaryTreeNode(new_value)
+    elif new_value < root.data:
+        root.left = insert(root.left, new_value)
+    else: #new_value >= root.data:
+        root.right = insert(root.right, new_value)
+    return root        
+
+
+def inorder(root) -> None:
+    if root: 
+        inorder(root.left)
+        print(root.data, end = " ")
+        inorder(root.right)
+
+
+def preorder(root) -> None:
+    if root:
+        print(root.data, end = " ")
+        preorder(root.left)
+        preorder(root.right)
+
+
+def postorder(root) -> None:
+    if root:
+        postorder(root.left)
+        postorder(root.right)
+        print(root.data, end = " ")
 
 
 # Do not change the following code
 input_data = input()
-data = []
+flag = True
+root = None
 for item in input_data.split(', '):
-  if item.isnumeric():
-    data.append(int(item))
-  elif item.lstrip("-").isnumeric():
-    data.append(int(item))
-merge_sort(data)
-print(data)
+    if flag is True:
+        root = insert(None, int(item))
+        flag = False
+    else:
+        insert(root, int(item))
+inorder(root)
+print()
+preorder(root)
+print()
+postorder(root)
